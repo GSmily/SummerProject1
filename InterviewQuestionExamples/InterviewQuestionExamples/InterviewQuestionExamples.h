66db9a333535979c1cb47e708486cf4c86772079
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2018-04-09 21:44:35 -0600

    Created platform to work on. Includes an example DLL function and Unit Test function.

---
+++ InterviewQuestionExamples/InterviewQuestionExamples/InterviewQuestionExamples.h
@@ -0,0 +1,12 @@
+// InterviewQuestionExamples.h - Contains functions that solve various interview questions
+#pragma once
+
+#ifdef INTERVIEWQUESTIONEXAMPLES_EXPORTS
+#define INTERVIEWQUESTIONEXAMPLES_API __declspec(dllexport)
+#else
+#define INTERVIEWQUESTIONEXAMPLES_API __declspec(dllimport)
+#endif
+
+extern "C++" INTERVIEWQUESTIONEXAMPLES_API unsigned long long fibonacci(
+	const unsigned long long n
+);
---
+++ InterviewQuestionExamples/InterviewQuestionExamplesTests/UnitTests.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\InterviewQuestionExamples\InterviewQuestionExamples.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InterviewQuestionExamplesTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+
+		TEST_METHOD(FibonnaciTest)
+		{
+			std::vector<unsigned long long> smallFibonnaciTestSequence = {
+				0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 
+				987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 
+				121393, 196418, 317811
+			};
+
+			for (unsigned long long i = 0; i < smallFibonnaciTestSequence.size(); ++i) {
+				Assert::AreEqual(smallFibonnaciTestSequence[i], fibonacci(i));
+			}
+		}
+
+	};
+}
